d8e33acf218f2cab0d7eb7f65c4d555f865b6108
jwinton@veroskills.com <jwinton@veroskills.com>
2022-09-07 11:02:34 -0700

    refactor: update responsive header styles

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,5 +1,3 @@
-import { ArrowTopRightIcon } from '@radix-ui/react-icons';
-
 import { Link } from '../../shared/link/Link';
 
 import { trpc } from 'client-data/utils/trpc';
@@ -8,14 +6,14 @@ import { RiSpotifyFill } from 'react-icons/ri';
 const NowPlaying = () => {
   const { data: spotify } = trpc.useQuery(['spotify.get-current-track']);
   return (
-    <div className="group flex gap-2">
+    <div className="group flex items-center gap-2">
       <RiSpotifyFill className="h-6 w-6 text-spotify" />
       {spotify && spotify?.track?.isPlaying ? (
         <Link to={spotify.track.songUrl} className="!block">
-          <span className="block text-sm">{spotify.track?.title}</span>
           <span className="block self-start text-xs text-accent-orange">
             {spotify.track?.artist}
           </span>
+          <span className="block text-sm">{spotify.track?.title}</span>
         </Link>
       ) : (
         <span className="text-sm">Not Playing</span>
@@ -30,11 +28,9 @@ export const Header = ({ upNext, link }: { upNext: string; link: string }) => {
       <div className="flex w-full items-center justify-between">
         <NowPlaying />
 
-        <div className="flex uppercase">
-          <span className="text-accent-orange">Up Next – &nbsp;</span>
-          <Link to={link || '#'} icon={<ArrowTopRightIcon />}>
-            {upNext}
-          </Link>
+        <div className="flex flex-col text-right">
+          <span className="text-xs text-accent-orange">Up Next</span>
+          <Link to={link || '#'}>{upNext}</Link>
         </div>
       </div>
     </header>
