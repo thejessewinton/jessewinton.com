010435d2bc213919cdaabc73b0e3fb1a67e21736
jwinton@veroskills.com <jwinton@veroskills.com>
2022-12-02 15:15:46 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@
  * @param {T} config - A generic parameter that flows through to the return type
  * @constraint {{import('next').NextConfig}}
  */
+function defineNextConfig(config) {
+  return config;
+}
+
 export default defineNextConfig({
   reactStrictMode: true,
   swcMinify: true,
