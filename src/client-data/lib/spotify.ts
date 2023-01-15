92a274da18d3e665210fd0be5725ab4d40a2e396
jwinton@veroskills.com <jwinton@veroskills.com>
2023-01-15 13:46:56 -0500

    fix

--- src/client-data/lib/spotify.ts
+++ src/client-data/lib/spotify.ts
@@ -1,26 +1,55 @@
-import { env } from 'env/server.mjs';
-import { SpotifyWebApi } from 'spotify-web-api-ts';
+import { env } from "env/server.mjs";
 
-export const spotifyApi = new SpotifyWebApi({
-  clientId: env.SPOTIFY_CLIENT_ID,
-  clientSecret: env.SPOTIFY_CLIENT_SECRET,
-});
-
-export const authorize = async () => {
-  const { access_token } = await spotifyApi.getRefreshedAccessToken(
-    env.SPOTIFY_REFRESH_TOKEN
-  );
-  spotifyApi.setAccessToken(access_token);
-
-  console.log('Spotify access token refreshed', access_token);
-
-  return access_token;
+type GetNowPlayingResponse = {
+  status: number;
+  is_playing: boolean;
+  item: {
+    name: string;
+    album: {
+      name: string;
+      images: Array<{ url: string }>;
+      artists: Array<{ name: string }>;
+    };
+    external_urls: {
+      spotify: string;
+    };
+  };
 };
 
-export const getMyCurrentPlayingTrack = async () => {
-  await authorize();
+const basic = Buffer.from(
+  `${env.SPOTIFY_CLIENT_ID}:${env.SPOTIFY_CLIENT_SECRET}`
+).toString("base64");
 
-  const response = await spotifyApi.player.getCurrentlyPlayingTrack();
+const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+const refresh_token = env.SPOTIFY_REFRESH_TOKEN as string;
 
-  return response;
+const getAccessToken = async () => {
+  const body = new URLSearchParams({
+    grant_type: "refresh_token",
+    refresh_token,
+  }).toString();
+
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${basic}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body,
+  });
+
+  return response.json() as Promise<{ access_token: string }>;
+};
+
+export const getNowPlaying = async () => {
+  const { access_token } = await getAccessToken();
+
+  const res = await fetch(NOW_PLAYING_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+
+  return (await res.json()) as GetNowPlayingResponse;
 };
