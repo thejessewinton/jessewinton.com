56eaf1d82151d360ade4c4350f085842d610ac86
jwinton@veroskills.com <jrandallwinton@gmail.com>
2023-02-28 17:21:35 -0500

    make blur work on light mode

--- src/components/blur/Blur.tsx
+++ src/components/blur/Blur.tsx
@@ -1,5 +1,5 @@
 export const Blur = () => {
   return (
-    <div className="pointer-events-none sticky top-0 z-[100] h-24 w-full select-none opacity-95 backdrop-blur-sm gradient-mask-b-0 after:absolute after:inset-0 after:bg-neutral-900/50" />
+    <div className="after:darrk:bg-neutral-900/50 pointer-events-none sticky top-0 z-[100] h-24 w-full select-none opacity-95 backdrop-blur-sm gradient-mask-b-0 after:absolute after:inset-0 after:bg-white/50" />
   );
 };
