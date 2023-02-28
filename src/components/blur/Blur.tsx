27da74b3d0372f6399ad674459569be5ea3a4c53
jwinton@veroskills.com <jrandallwinton@gmail.com>
2023-02-28 18:47:36 -0500

    fix height on blur

--- src/components/blur/Blur.tsx
+++ src/components/blur/Blur.tsx
@@ -1,5 +1,5 @@
 export const Blur = () => {
   return (
-    <div className="pointer-events-none sticky top-0 z-[100] h-24 w-full select-none opacity-95 backdrop-blur-sm gradient-mask-b-0 after:absolute after:inset-0 after:bg-white/50 dark:after:bg-neutral-900/50" />
+    <div className="pointer-events-none sticky top-0 z-[100] h-16 w-full select-none opacity-95 backdrop-blur-sm gradient-mask-b-0 after:absolute after:inset-0 after:bg-white/50 dark:after:bg-neutral-900/50" />
   );
 };
