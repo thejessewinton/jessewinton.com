b1505a41c75ed585001985d5621d63ab7593a889
Jesse Winton <jrandallwinton@gmail.com>
2024-02-12 00:16:13 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,20 +26,24 @@ export default function Index() {
 
       {index.works ? (
         <div className='group pointer-events-none relative mb-12 mt-8 grid animate-enter gap-12 animation-delay-300 sm:grid-cols-3'>
-          {index.works.map((work) => (
-            <Link
-              key={work.label}
-              href={work.url || ''}
-              target={work.url?.startsWith('http') ? '_blank' : '_self'}
-              className='pointer-events-auto relative min-h-[90px] font-light transition-all hover:md:!opacity-100 hover:md:!blur-none group-hover:md:opacity-40 group-hover:md:blur-xs'
-            >
-              <h2 className='mb-4 text-sm text-neutral-900 dark:text-neutral-400'>{work.label}</h2>
-              <div className='gap-6'>
-                <span className='font-normal'>{work.title}</span>
-                <div className='text-neutral-900 dark:text-neutral-400'>{work.description}</div>
-              </div>
-            </Link>
-          ))}
+          {index.works.map((work) => {
+            const isExternal = work.url?.startsWith('https')
+
+            return (
+              <Link
+                key={work.label}
+                href={work.url || ''}
+                target={isExternal ? '_blank' : '_self'}
+                className='pointer-events-auto relative min-h-[90px] font-light transition-all hover:md:!opacity-100 hover:md:!blur-none group-hover:md:opacity-40 group-hover:md:blur-xs'
+              >
+                <h2 className='mb-4 text-sm text-neutral-900 dark:text-neutral-400'>{work.label}</h2>
+                <div className='gap-6'>
+                  <span className='font-normal'>{work.title}</span>
+                  <div className='text-neutral-900 dark:text-neutral-400'>{work.description}</div>
+                </div>
+              </Link>
+            )
+          })}
         </div>
       ) : null}
     </div>
