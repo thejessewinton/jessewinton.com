6c2498cb1a500e5872f0ab8ede84bf6ae325a69f
Jesse Winton <jrandallwinton@gmail.com>
2024-02-17 11:53:43 -0500

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,8 +1,6 @@
-import { classNames } from '~/utils/classnames'
+import type { SVGProps } from 'react'
 
-const defaultClasses = 'h-4 w-4 text-neutral-400'
-
-export const Arrow = ({ className }: { className?: string }) => {
+export const Arrow = (props: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
@@ -10,28 +8,9 @@ export const Arrow = ({ className }: { className?: string }) => {
       viewBox='0 0 24 24'
       strokeWidth={1.5}
       stroke='currentColor'
-      className={classNames(defaultClasses, className)}
+      {...props}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25' />
-    </svg>
-  )
-}
-
-export const Document = () => {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      fill='none'
-      viewBox='0 0 24 24'
-      strokeWidth={1.5}
-      stroke='currentColor'
-      className={defaultClasses}
-    >
-      <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z'
-      />
+      <path strokeLinecap='round' strokeLinejoin='round' d='M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3' />
     </svg>
   )
 }
