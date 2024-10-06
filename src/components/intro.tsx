7a4a227150d930962260aa4ddcad53ec9a0b0ff6
Jesse Winton <jesse@appwrite.io>
2024-10-06 19:25:54 -0400

    updat e

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -2,7 +2,7 @@
 
 import { useMousePosition } from '~/hooks/use-mouse-position'
 import Image from 'next/image'
-import Headshot from '~/public/images/headshot.webp'
+import Me from '~/public/images/me.webp'
 
 export const Intro = () => {
   const position = useMousePosition()
@@ -10,8 +10,8 @@ export const Intro = () => {
   return (
     <div className='hidden md:inline'>
       <Image
-        src={Headshot}
-        alt={''}
+        src={Me}
+        alt='4 year old Jesse sitting in a baller bow tie.'
         className='absolute right-0 hidden animate-fade-blur shadow-xl group-hover:block'
         style={{
           transform: position.x && position.y ? `translate(${position.x / 3}px, ${position.y / 8}px)` : undefined
